a3776b4e3e77857669440cf6a53108eeb77abd2e
Sigri44 <sigri44@hotmail.fr>
2018-12-05 11:30:16 +0100

    Exemple code session Servlet

--- src/fr/eni/servlets/Connexion.java
+++ src/fr/eni/servlets/Connexion.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Connexion")
 public class Connexion extends HttpServlet {
@@ -16,6 +17,18 @@ public class Connexion extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/jsp/connexion.jsp" ).forward( request, response );
+		
+		/* Création ou récupération de la session */
+		HttpSession session = request.getSession();
+
+		/* Mise en session d'une chaîne de caractères */
+		String exemple = "abc";
+		session.setAttribute( "chaine", exemple );
+
+		/* Récupération de l'objet depuis la session */
+		String chaine = (String) session.getAttribute( "chaine" );
+		
+		System.out.println(chaine);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
